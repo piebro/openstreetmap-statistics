9c30515ffdd3367ce2da333fafb764351634ea09
Piet <piet.broemmel@gmail.com>
2022-11-25 22:13:45 +0100

    added two more contributor plots

--- src/add_topic_general.py
+++ src/add_topic_general.py
@@ -7,13 +7,18 @@ data_dir = sys.argv[1]
 months, years = util.get_months_years(data_dir)
 year_to_year_index = util.list_to_dict(years)
 month_index_to_year_index = {month_i: year_to_year_index[month[:4]] for month_i, month in enumerate(months)}
+tag_to_id = util.list_to_dict(util.load_index_to_tag(data_dir, "created_by"))
+maps_me_android_id = tag_to_id["MAPS.ME android"]
+maps_me_ios_id = tag_to_id["MAPS.ME ios"]
+
 
 mo_ch = np.zeros((len(months)), dtype=np.int64)
 mo_ed = np.zeros((len(months)), dtype=np.int64)
 ye_map_ed = np.zeros((len(years), 360, 180), dtype=np.int64)
 total_map_ed = np.zeros((360, 180), dtype=np.int64)
 mo_co_set = [set() for _ in range(len(months))]
-
+mo_co_set_without_maps_me = [set() for _ in range(len(months))]
+mo_co_edit_count = [dict() for _ in range(len(months))]
 
 # accumulate data
 for line in sys.stdin:
@@ -22,17 +27,26 @@ for line in sys.stdin:
     month_index = int(data[1])
     user_id = int(data[2])
     x, y = data[4], data[5]
-    
+
     mo_ch[month_index] += 1
     mo_ed[month_index] += edits
     mo_co_set[month_index].add(user_id)
+    if user_id not in mo_co_edit_count[month_index]:
+        mo_co_edit_count[month_index][user_id] = 0    
+    mo_co_edit_count[month_index][user_id] += edits
+
     if len(x)>0:
         ye_map_ed[month_index_to_year_index[month_index], int(x), int(y)] += edits
         total_map_ed[int(x), int(y)] += edits
 
+    if len(data[7])==0 or (int(data[7]) != maps_me_android_id and int(data[7]) != maps_me_ios_id):
+        mo_co_set_without_maps_me[month_index].add(user_id)
+        
 
 mo_co = util.set_to_length(mo_co_set)
+mo_co_without_maps_me = util.set_to_length(mo_co_set_without_maps_me)
 ye_map_ed_max_z_value = np.max(ye_map_ed)
+mo_co_edit_count_higher_then_100 = [np.sum(np.array(list(mo_co_edit_count[month_index].values()))>100) for month_index in range(len(months))]
 
 # save plots
 topic = "General"
@@ -41,7 +55,14 @@ with open("assets/data.js", "a") as f:
 
     question = "How many people are contributing each month?"
     f.write(util.get_js_str(topic, question, "63f6", [
-        util.get_single_line_plot("contributors per month", "contributors", months, mo_co)
+        util.get_single_line_plot("contributors per month", "contributors", months, mo_co),
+        util.get_single_line_plot("contributors with more then 100 edits per month", "contributors", months, mo_co_edit_count_higher_then_100),
+    ]))
+
+    question = "Why is there rapid growth in monthly contributors in 2016?"
+    f.write(util.get_js_str(topic, question, "21d9", [
+        ("text", "That's because a lot of new people were contributing using the maps.me app. Looking at the plot of monthly contributors not using maps.me shows that there is linear growth."),
+        util.get_single_line_plot("contributors per month without maps.me contributors", "contributors", months, mo_co_without_maps_me),
     ]))
 
     question = "How many edits are added each month?"
