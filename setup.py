f5b77ed543edd4500bc34fb20ce10b9c72474377
Piet <piet.broemmel@gmail.com>
2023-05-08 14:14:02 +0200

    linter

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from pathlib import Path
 parent_dir = Path(__file__).resolve().parent
 
 setup(
-    name='openstreetmapStatistics',
-    version='0.0.1',
-    
+    name="openstreetmapStatistics",
+    version="0.0.1",
     description="A python library to create statistics about OpenStreetMap using OSM-Changesets.",
     url="https://github.com/piebro/openstreetmap-statistics/",
     author="Piet Brömmel",
     license="MIT License",
     install_requires=parent_dir.joinpath("requirements.txt").read_text().splitlines(),
-    packages=['openstreetmapStatistics'],
+    packages=["openstreetmapStatistics"],
 )
